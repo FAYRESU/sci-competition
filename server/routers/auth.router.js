64f1c2df43152303594ff1f8b1f253a7ed587002
FAYRESU <664259005@webmail.npru.ac.th>
2025-09-25 01:28:08 +0700

    console debug register

--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -1,14 +1,12 @@
 import express from "express";
 const router = express.Router();
 import authController from "../controllers/auth.controller.js";
-
-// POST: http://localhost:5000/api/v1/auth/signUp
+//POST http://localhost:5000/api/v1/auth/signup
 router.post("/signup", authController.signUp);
 
-// GET: http://localhost:5000/api/v1/auth/verify/:token
+//GET http://localhost:5000/api/v1/auth/verify/:token
 router.get("/verify/:token", authController.verifyEmail);
 
-// POST: http://localhost:5000/api/v1/auth/signin
-// router.post("/signin", authController.signIn);
-
+//POST http://localhost:5000/api/v1/auth/signin
+router.post("/signin", authController.signIn);
 export default router;
